ce9d005dea21b064ddaecd1516d17dfc9a2262bd
Jan Axelsson <axelsson.jan@gmail.com>
2022-10-09 21:11:33 +0200

    ctc_logger -- logs to "/var/log/ctc_log.txt"

--- ctc_logger.py
+++ ctc_logger.py
@@ -1,15 +1,73 @@
-import time
 import jan_heatpump_module as ctc
-import jan_shelly_module as plug
+import traceback
+import time
+import datetime
 
-status = ctc.compressor_status()
-rpm = ctc.compressor_rpm()
-radiator_return = ctc.radiator_return()
-rad_out = ctc.radiator_out()
-outdoor_temp = ctc.outdoor_temp()
-warm_water = ctc.warm_water_percent()
-brine_in = ctc.brine_in()
-brine_out = ctc.brine_out()
+LOGFILE = "/var/log/ctc_log.txt"  # On mac : sudo touch, and, sudo chmod a+w
+DELAY = 60
 
-print(status)
+EOL = '\n'
+TAB = '\t'
+header = 'time        ' + TAB + \
+    'date          ' + TAB + \
+    'out_T' + TAB + \
+    'VS/VV' + TAB + \
+    'status' + TAB + \
+    'rpm' + TAB +  \
+    'rad_ret' + TAB + \
+    'rad_out' + TAB + \
+    'brineIn' + TAB + \
+    'brineUt' + TAB + \
+    'brineDt' + TAB + \
+    'bri/rpm'
+
+f = open( LOGFILE, "a", buffering=1)
+f.write(header + EOL)
+
+while True:
+    try:
+        time.sleep(DELAY)
+
+        now = datetime.datetime.now()
+        date = now.strftime("%Y-%m-%d")
+        time_now = now.strftime("%H:%M:%S")
+
+        status = ctc.compressor_status()
+        rpm = ctc.compressor_rpm()
+        heater = ctc.heat_system()
+        radiator_return = ctc.radiator_return()
+        rad_out = ctc.radiator_out()
+        outdoor_temp = ctc.outdoor_temp()
+        warm_water = ctc.warm_water_percent()
+        brine_in = ctc.brine_in()
+        brine_out = ctc.brine_out()
+        brine_dT = "{:6.4f}".format(  float(brine_out) - float(brine_in) )
+
+        brineDTOverRPM = '0'
+        if float(rpm) > 10:
+            brineDTOverRPM = "{:6.4f}".format(  -100 * ( float(brine_out) - float(brine_in) ) / float(rpm) )
+
+
+        data = time_now + TAB + \
+            date + TAB + \
+            outdoor_temp + TAB + \
+            heater + TAB + \
+            status + TAB + \
+            rpm + TAB + \
+            radiator_return + TAB + \
+            rad_out + TAB + \
+            brine_in + TAB + \
+            brine_out + TAB + \
+            brine_dT + TAB + \
+            brineDTOverRPM + EOL
+        f.write( data)
+
+    except Exception as e:
+        print('error')
+        print(e)
+        traceback.print_exc()
+
+
+f.close()
+
 
--- jan_heatpump_module.py
+++ jan_heatpump_module.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 BASEURL = 'http://192.168.2.3'
 
@@ -6,31 +7,48 @@ DRIFTINFO = BASEURL + '/click/glob;menu;1;0;112;113'  # First level Drift info o
 DRIFTINFO_PUMP = BASEURL + '/click/glob;menu;1;0;111'  # submenu on CTC GIS 212 display
 DRIFTINFO_VV_O_EL = BASEURL + '/click/glob;menu;1;0;110'  # submenu on CTC GIS 212 display
 
+def print(a): # NOTE : override print.  Comment out to print output for all functions
+    return
 
 def query_ctc_menu(url, pos, description, output):
+
     headers = {'Content-Type': 'application/x-www-form-urlencoded', }
     data = '-1,-1'
-    response = requests.post(url, headers=headers, data=data)
+    response = requests.post(url, headers=headers, data=data, timeout = 10)
 
     response_items = response.text.replace('\r', '').split('|')  # Replace CR with ''
     if output:
         print(description + ' = ' + str( float(response_items[pos]) / 10) )
 
-    return float(response_items[pos]) / 10
+    answer = str( float(response_items[pos]) / 10 )
+
+    return answer
 
 def compressor_status():
     status_value = query_ctc_menu(DRIFTINFO_PUMP, 64, 'Compressor status', False) # Divided by 10
-    if status_value == 1.4 :
+    if str(status_value) == '1.4' :
         print('Compressor status = off')
         return 'off'
 
-    if status_value == 1.5 :
+    if str(status_value)  == '1.5' :
         print('Compressor status = disabled')
         return 'disabled'
 
-    if status_value == 0.1 :
+    if str(status_value)  == '0.1' :
         print('Compressor status = on')
         return 'on'
+
+    return 'unknown'
+
+def heat_system():
+    status_value = query_ctc_menu(DRIFTINFO_VV_O_EL, 59, 'VS/VV', False) # Divided by 10
+    if str(status_value) == '0.5' :
+        print('Heating = VS')
+        return 'VS'
+
+    if str(status_value)  == '0.6' :
+        print('Heating = VV')
+        return 'VV'
 
     return 'unknown'
 
